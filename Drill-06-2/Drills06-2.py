ab71845d8a6d0f3ce15aec09b7bd8a43ea592863
Leedonghee(Notebook) <jjaing01@naver.com>
2019-10-09 18:44:40 +0900

    Drills06-2 3차 진행중
    2015182030 / 이동희 / Drills06-2 / 3차커밋 진행중
    커밋횟수 : 3

--- Drill-06-2/Drills06-2.py
+++ Drill-06-2/Drills06-2.py
@@ -28,6 +28,37 @@ def move_point(p):
     frame = (frame + 1) % 8
     delay(0.05)
     handle_events()
+    
+    
+def draw_start_end(p1, p2, p3, near, far):
+    for i in range(near, far, 2):
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+        move_point((x, y))
+
+
+def draw_middle(p1, p2, p3, p4, near, far):
+    direct_character(p2, p3)
+
+    for i in range(near, far, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (-3 * t ** 3 + 4 * t ** 2 + t)
+             * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (-3 * t ** 3 + 4 * t ** 2 + t)
+             * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+        move_point((x, y))
+        
+def direct_character(p1, p2):
+    global dir
+
+    if p1[0] < p2[0]:
+        dir = 1
+    elif p1[0] > p2[0]:
+        dir = 0
+
+
+
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
