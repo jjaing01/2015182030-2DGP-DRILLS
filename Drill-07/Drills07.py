0f9197abfdc40eb40e7e43dc77399fc16489bec4
Leedonghee(Notebook) <jjaing01@naver.com>
2019-10-14 18:45:02 +0900

    Drills07 2차 진행중
    2015182030 이동희 Drills07
    커밋횟수 :  2

--- Drill-07/Drills07.py
+++ Drill-07/Drills07.py
@@ -26,32 +26,6 @@ class Boy:
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
 
-class Ball:
-    def __init__(self):
-        self.x, self.y = random.randint(10, 790), 700
-        self.ball_size = random.randint(0, 1)
-        self.speed = random.randint(5, 20)
-        if self.ball_size == 0:
-            self.image = load_image('ball21x21.png')
-        elif self.ball_size == 1:
-            self.image = load_image('ball41x41.png')
-
-    def update(self):
-        if self.ball_size == 0:
-            if self.y <= 64:
-                self.y = 64
-            else:
-                self.y -= self.speed
-        elif self.ball_size == 1:
-            if self.y <= 74:
-                self.y = 74
-            else:
-                self.y -= self.speed
-
-    def draw(self):
-        self.image.draw(self.x, self.y)
-
-
 def handle_events():
     global running
     events = get_events()
@@ -63,43 +37,29 @@ def handle_events():
 
 
 # initialization code
-
-
 open_canvas()
 
 team = [Boy() for i in range(11)]
-balls = [Ball() for j in range(20)]
 grass = Grass()
 
 running = True
 
 # game main loop code
-
-
 while running:
     handle_events()
 
     for boy in team:
         boy.update()
 
-    for ball in balls:
-        ball.update()
-
     clear_canvas()
 
     for boy in team:
         boy.draw()
 
-    for ball in balls:
-        ball.draw()
-
     grass.draw()
     update_canvas()
 
     delay(0.05)
-
-
 # finalization code
-
 
 close_canvas()
