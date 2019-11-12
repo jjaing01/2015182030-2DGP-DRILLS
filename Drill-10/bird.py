5c95ad394de3f0c38394316706e76ca5ccc71bbc
Leedonghee(Notebook) <jjaing01@naver.com>
2019-11-12 20:47:17 +0900

    Drills10-2 (Finish)
    https://youtu.be/ZX18X_dsYBs
    2015182030 이동희
    커밋횟수 : 2

--- Drill-10/bird.py
+++ Drill-10/bird.py
@@ -1,67 +1,77 @@
 import game_framework
 from pico2d import *
-import time
 
 import game_world
-
-# fill expressions correctly
-PIXEL_PER_METER =  (10.0 / 0.5)
-RUN_SPEED_KMPH =  40.0
-RUN_SPEED_MPM =  (RUN_SPEED_KMPH * 1000.0 / 60.0)
-RUN_SPEED_MPS = (RUN_SPEED_MPM * 60.0)
+PIXEL_PER_METER = (10.0/0.3)
+RUN_SPEED_KMPH = 50.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0/60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM/60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-TIME_PER_ACTION = 2.0
+TIME_PER_ACTION = 1
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 14
+
 
 class Bird:
 
     def __init__(self):
-        self.x, self.y = 1600 // 2, 300
+        self.x, self.y = 1600 // 2, 90
+        self.font = load_font('ENCR10B.TTF',16)
+
         self.image = load_image('bird_animation.png')
         # fill here
-        #self.font = load_font('ENCR10B.TTF', 16)
         self.dir = 1
-        self.velocity = RUN_SPEED_PPS
-        self.frameX = 0
-        self.frameY = 0
-        self.frameTime=0
-        self.current_time = time.time()
+        self.velocity = 0
+        self.frame = 0
+        self.framex = 0
+        self.framey = 0
 
+        self.num=0
+
+
+
+    def add_event(self, event):
+        pass
     def update(self):
+        self.velocity=0
+        if self.dir ==1:
+            self.velocity += RUN_SPEED_PPS
+        elif self.dir ==-1:
+            self.velocity -= RUN_SPEED_PPS
+        self.num += 1
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
 
-        #print("Frame Time: %f sec" % ( game_framework.frame_time))
         self.x += self.velocity * game_framework.frame_time
-        if self.dir==1:
-            self.velocity = RUN_SPEED_PPS
-            if self.x>1500:
-                self.dir=-1
-        elif self.dir==-1:
-            self.velocity = -RUN_SPEED_PPS
-            if self.x <100:
-                self.dir = 1
 
-        self.frameTime+=game_framework.frame_time
-        if self.frameTime>0.1:
-            self.frameTime=0
-            self.frameX+=1
+        self.x = clamp(0, self.x, 1600)
+        if self.x == 1600:
+            self.dir =-1
+        elif self.x == 0:
+            self.dir = 1
 
-        if self.frameX==5:
-            self.frameX=0
-            self.frameY+=1
-        if self.frameY == 3:
-            self.frameY = 0
+        self.framex = int(self.frame) % 5
+        if int(self.frame) % 14 == 0:
+            self.framey = 2
+        if int(self.frame) % 14 == 5:
+            self.framey = 1
+        if int(self.frame) % 14 == 10:
+            self.framey = 0
+        #print('frame : %d' % (self.frame % 14))
+       # print('framex : %d' % (self.framex))
+        #print('framey : %d' % (self.framey))
+        print(RUN_SPEED_PPS)
+
 
         pass
 
     def draw(self):
-        if self.dir==1:
-            self.image.clip_composite_draw(self.frameX * 183, self.frameY * 168, 180, 166,
-                                            0.0, 'h', self.x, self.y, 100, 100)
-
-        if self.dir == -1:
-            self.image.clip_draw(self.frameX * 183, self.frameY * 168, 183, 168, self.x, self.y, 100, 100)
+        self.font.draw(self.x-60, self.y+50, '(Time: %3.2f)' %get_time(), (255,255,0))
+        if self.dir == 1:
+            self.image.clip_composite_draw(self.framex * 182, self.framey * 168, 183, 168,0, '', self.x, 300,100,100)
+        elif self.dir == -1:
+            self.image.clip_composite_draw(self.framex * 182, self.framey * 168, 183, 168, 3.141592 , 'v', self.x, 300, 100,100)
         pass
 
-
+    def handle_event(self, event):
+        pass
