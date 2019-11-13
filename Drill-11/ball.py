7faca101f9fe37f6e4d524b0ef797bfdafe14989
Leedonghee(Notebook) <jjaing01@naver.com>
2019-11-14 01:23:34 +0900

    Drills11 (3차 커밋)
    커밋횟수 3

--- Drill-11/ball.py
+++ Drill-11/ball.py
@@ -2,18 +2,24 @@ import random
 from pico2d import *
 import game_world
 import game_framework
+import main_state
+
 
 class Ball:
     image = None
 
     def __init__(self):
-        if Ball.image == None:
+        if Ball.image is None:
             Ball.image = load_image('ball21x21.png')
-        self.x, self.y, self.fall_speed = random.randint(0, 1600-1), 60, 0
+        self.x, self.y, self.fall_speed = random.randint(0, 1600 - 1), 60, 0
+        self.Coli = False
+        self.Brick = main_state.brick
+        self.TermX = 0
+        self.Init = False
 
     def get_bb(self):
         # fill here
-        return self.x-10, self.y-10, self.x+10, self.y+10
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
 
     def draw(self):
         self.image.draw(self.x, self.y)
@@ -21,26 +27,33 @@ class Ball:
         draw_rectangle(*self.get_bb())
 
     def update(self):
-        self.y -= self.fall_speed * game_framework.frame_time
+        if self.Coli is False:
+            self.y -= self.fall_speed * game_framework.frame_time
 
-    #fill here for def stop
+        if self.Coli is True and self.Init is True:
+            self.x = self.Brick.x + self.TermX
+            self.y = self.Brick.y + 40
+
+    # fill here for def stop
     def stop(self):
         self.fall_speed = 0
 
 
 # fill here
+# class BigBall
 class BigBall(Ball):
-    MIN_FALL_SPEED = 50
-    MAX_FALL_SPEED = 200
+    MIN_FALL_SPEED = 50  # 50 pps = 1.5 meter per sec
+    MAX_FALL_SPEED = 200  # 200 pps = 6 meter per sec
     image = None
 
     def __init__(self):
-        if BigBall.image == None:
+        if BigBall.image is None:
             BigBall.image = load_image('ball41x41.png')
-
-        self.x,self.y=random.randint(0,1600-1),500
-        self.fall_speed = random.randint(BigBall.MIN_FALL_SPEED,
-                                       BigBall.MAX_FALL_SPEED)
+        self.x, self.y = random.randint(600, 1400 - 1), 500
+        self.fall_speed = random.randint(100, 120)
+        self.Coli = False
+        self.Brick = main_state.brick
+        self.Init = False
 
     def get_bb(self):
-        return self.x-20, self.y-20, self.x+20, self.y+20
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
--- Drill-11/brick.py
+++ Drill-11/brick.py
@@ -3,29 +3,31 @@ from pico2d import *
 import game_world
 import game_framework
 
+
 class Brick:
     image = None
 
     def __init__(self):
-        if Brick.image == None:
+        if Brick.image is None:
             Brick.image = load_image('brick180x40.png')
-        self.x, self.y = random.randint(0, 1600-1), 300
+        self.x, self.y = 1600 // 2, 300
+        self.speed = 200
         self.dir = 1
-        self.speed = 100
 
     def get_bb(self):
         # fill here
-        return self.x-10, self.y-10, self.x+10, self.y+10
+        return self.x - 90, self.y - 20, self.x + 90, self.y + 20
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        self.image.draw(self.x, self.y, 180, 40)
         # fill here for draw
         draw_rectangle(*self.get_bb())
 
     def update(self):
-        if(self.x > 1600 or self.x < 0):
-            self.dir = self.dir * -1
+        self.x += self.speed * game_framework.frame_time * self.dir
 
-        self.x += self.dir * game_framework.frame_time * self.speed
-
-
+        clamp(0, self.x, 1600)
+        if self.x < 0:
+            self.dir = 1
+        elif self.x >= 1600:
+            self.dir = -1
